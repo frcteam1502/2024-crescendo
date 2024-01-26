2506ba6b14025a0f6284cbaab0abd74ecb97aca9
jonathan berent <jonathan.berent@vsolvit.com>
2024-01-25 20:48:03 -0500

    break config into reusable units

--- src/main/java/team1502/configuration/configurations/RobotConfigurations.java
+++ src/main/java/team1502/configuration/configurations/RobotConfigurations.java
@@ -9,93 +9,102 @@ public final class RobotConfigurations {
 
     public static RobotConfiguration getConfiguration(String radio) {
         switch(radio) {
-            case "1502": return createRobot2024();
-            default: return createRobot2024();
+            case "1502": return RobotConfiguration.Create("robot_2024_1502", r->createRobot2024(r));
+            default: return RobotConfiguration.Create("robot_2024_1502", r->createRobot2024(r));
         }
     }
+
+    private static RobotConfiguration createRobot2024(RobotConfiguration configuration) {
+        buildCompetitionBot(standardChassis(configuration));
+        addEvalHelpers(configuration);
+        return configuration;
+    }
     
-    public static RobotConfiguration createRobot2024() { return RobotConfiguration
-        .Create("robot_2024_Test", r -> r
-            // Inventory Definitions
-            .Parts(define -> define
-                .GyroSensor("Pigeon2", Manufacturer.CTRElectronics, p -> p
-                    .Reversed(true) // no longer used? what did it mean?
-                )
-                .Motor("NEO", m -> m
-                    .MotorType(MotorType.kBrushless)
-                    .FreeSpeedRPM(5_820.0) // from MK4i docs, see data sheet for empirical values
-                    .Note("NAME", "NEO")
-                    .Note("VERSION", "V1.0/V1.1")
-                    .Note("gearing", "8mm bore pinion gears")
-                    .Note("DATA SHEET", "https://www.revrobotics.com/content/docs/REV-21-1650-DS.pdf")
-                    .Value("empiricalFreeSpeed", 5_676.0) // how to choose best vaule?
-                )
-                .SwerveModule(sm -> sm
-                    .Encoder(Manufacturer.REVRobotics, cc -> cc
-                        .Direction(false)
-                    )
-                    .TurningMotor(Manufacturer.REVRobotics, mc -> mc
-                        .Motor("NEO")
-                        .IdleMode(IdleMode.kCoast)
-                        .GearBox(g-> g
-                            .Gear("Stage1", 14, 50)
-                            .Gear("Stage2", 10, 60)
-                            .Note("MK4i Standard", "150/7:1")
-                        )
-                        .PID(3.5, 0.0, 0.0)
-                        .Reversed(true) // all turn motors are reversed
-                    )
-                    .DrivingMotor(Manufacturer.REVRobotics, mc -> mc
-                        .Motor("NEO")
-                        .IdleMode(IdleMode.kBrake)
-                        .GearBox(g-> g
-                            .Gear("Stage1", 14, 50)
-                            .Gear("Stage2", 27, 17)
-                            .Gear("Stage3", 15, 45)
-                            .Note("Mk4i Option", "L2 = 6.75:1")
-                        )
-                        .PID(.08, 0.0, 0.0, 1.0)
-                         // no drive motors were reversed, but now left (?)
-                    )
-                    .Value("closedLoopRampRate", .25)
-                    .Value("smartCurrentLimit", 40)
-                )
-                .SwerveDrive(sd -> sd
-                    .Chassis(c -> c
-                        .Square(23.25)
-                        .WheelDiameter(4.0)
-                    )
-                    .Value("MAX_SPEED_METERS_PER_SECOND", 4.6) // hard-code or calc?
-                )
+    private static RobotConfiguration standardChassis(RobotConfiguration inventory) {
+        // Inventory Definitions
+        return inventory.Parts(define -> define
+            .GyroSensor("Pigeon2", Manufacturer.CTRElectronics, p -> p)
+            .Motor("NEO", m -> m
+                .MotorType(MotorType.kBrushless)
+                .FreeSpeedRPM(5_820.0) // from MK4i docs, see data sheet for empirical values
+                .Note("NAME", "NEO")
+                .Note("VERSION", "V1.0/V1.1")
+                .Note("gearing", "8mm bore pinion gears")
+                .Note("DATA SHEET", "https://www.revrobotics.com/content/docs/REV-21-1650-DS.pdf")
+                .Value("empiricalFreeSpeed", 5_676.0) // how to choose best vaule?
             )
-            // Top-Level Parts
-            .Build(hw -> hw
-                .GyroSensor("Gyro", "Pigeon2", g->g.CanNumber(14))
-                .SwerveDrive(sd -> sd
-                    .SwerveModule("Module#1", sm -> sm
-                        .CanNumberDown(16) // 16 16 15 -- also PDP channel
-                        .Encoder(e -> e.MagneticOffset(151.96))
-                        .DrivingMotor().Reversed(true) // left side reversed
-                    )
-                    .SwerveModule("Module#2", sm -> sm
-                        .CanNumber(10) // 10 10 11
-                        .Encoder(e -> e.MagneticOffset(121.81))
-                    )
-                    .SwerveModule("Module#3", sm -> sm
-                        .CanNumberDown(4) // 4 4 3
-                        .Encoder(e -> e.MagneticOffset(4.83))
-                        .DrivingMotor().Reversed(true) // left side reversed
-                    )
-                    .SwerveModule("Module#4", sm -> sm
-                        .CanNumber(8) // 8 8 9
-                        .Encoder(e -> e.MagneticOffset(127.26))
-                    )
-                    // miscellaneous
-                    .Value("goStraightGain", 0.02)
+            .SwerveModule(sm -> sm
+                .Encoder(Manufacturer.REVRobotics, cc -> cc
+                    .Direction(false)
                 )
+                .TurningMotor(Manufacturer.REVRobotics, mc -> mc
+                    .Motor("NEO")
+                    .IdleMode(IdleMode.kCoast)
+                    .GearBox(g-> g
+                        .Gear("Stage1", 14, 50)
+                        .Gear("Stage2", 10, 60)
+                        .Note("MK4i Standard", "150/7:1")
+                    )
+                    .PID(3.5, 0.0, 0.0)
+                    .Reversed(true) // all turn motors are reversed
+                )
+                .DrivingMotor(Manufacturer.REVRobotics, mc -> mc
+                    .Motor("NEO")
+                    .IdleMode(IdleMode.kBrake)
+                    .GearBox(g-> g
+                        .Gear("Stage1", 14, 50)
+                        .Gear("Stage2", 27, 17)
+                        .Gear("Stage3", 15, 45)
+                        .Note("Mk4i Option", "L2 = 6.75:1")
+                    )
+                    .PID(.08, 0.0, 0.0, 1.0)
+                        // no drive motors were reversed, but now left (?)
+                )
+                .Value("closedLoopRampRate", .25)
+                .Value("smartCurrentLimit", 40)
             )
-            // Configuration Values
-            .Values(k -> k
+            .SwerveDrive(sd -> sd
+                .Chassis(c -> c
+                    .Square(23.25)
+                    .WheelDiameter(4.0)
+                )
+                .Value("MAX_SPEED_METERS_PER_SECOND", 4.6) // hard-code or calc?
+            )
+        );
+    }
+
+    private static RobotConfiguration buildCompetitionBot(RobotConfiguration parts) {
+        // Top-Level Parts
+        return parts.Build(hw -> hw
+            .GyroSensor("Gyro", "Pigeon2", g->g.CanNumber(14))
+            .SwerveDrive(sd -> sd
+                .SwerveModule("Module#1", sm -> sm
+                    .CanNumberDown(16) // 16 16 15 -- also PDP channel
+                    .Encoder(e -> e.MagneticOffset(151.96))
+                    .DrivingMotor().Reversed(true) // left side reversed
+                )
+                .SwerveModule("Module#2", sm -> sm
+                    .CanNumber(10) // 10 10 11
+                    .Encoder(e -> e.MagneticOffset(121.81))
+                )
+                .SwerveModule("Module#3", sm -> sm
+                    .CanNumberDown(4) // 4 4 3
+                    .Encoder(e -> e.MagneticOffset(4.83))
+                    .DrivingMotor().Reversed(true) // left side reversed
+                )
+                .SwerveModule("Module#4", sm -> sm
+                    .CanNumber(8) // 8 8 9
+                    .Encoder(e -> e.MagneticOffset(127.26))
+                )
+                // miscellaneous
+                .Value("goStraightGain", 0.02)
+            )
+        );
+    }
+    
+    private static RobotConfiguration addEvalHelpers(RobotConfiguration robot) {
+        // Configuration Values
+        return robot.Values(k -> k
                 .Eval("Pigeon2", e->e.GyroSensor())
                 .Eval("SwerveModule.TurningMotor.Motor.MotorType", e -> e
                     .SwerveModule(e.partName(), m -> m.TurningMotor().Motor().MotorType()))
@@ -111,8 +120,6 @@ public final class RobotConfigurations {
                     .SwerveDrive(m -> m.getModules()))
                 .Eval("SwerveDrive.Kinematics", e -> e
                     .SwerveDrive(m -> m.getKinematics()))
-            )
-        );
-
+            );
     }
 }
