313ba8485fbda086d6607647f20701775cd8cc71
jonathan berent <jonathan.berent@vsolvit.com>
2024-01-26 21:40:55 -0500

    send deploy information (for example)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.testmode.swerve.AbsoluteEncoderAlignment;
@@ -25,6 +26,10 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
 
+  public String branch = "unknown";
+  public String commit = "unknown";
+  public String radio = "1502";
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -32,9 +37,6 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     Path deployPath = Filesystem.getDeployDirectory().toPath();
-    String branch = "unknown";
-    String commit = "unknown";
-    String radio = "1502";
     try {
       branch = Files.readString(deployPath.resolve("branch.txt"));
       commit = Files.readString(deployPath.resolve("commit.txt"));
@@ -73,6 +75,11 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    if (GameState.isFirst()) {
+      SmartDashboard.putString("Code Branch", branch);
+      SmartDashboard.putString("Code Commit", commit);
+      SmartDashboard.putString("Configuration (radio)", radio);
+    }
     GameState.robotPeriodic();
   }
 
