75dac448584c2472264585f3a174e0ce7974677a
jonathan berent <jonathan.berent@vsolvit.com>
2024-02-12 17:24:38 -0500

    WIP defaultCommand

--- src/main/java/team1502/configuration/annotations/SubsystemInfo.java
+++ src/main/java/team1502/configuration/annotations/SubsystemInfo.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import edu.wpi.first.wpilibj2.command.Command;
+
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SubsystemInfo {
     boolean disabled() default false;
+    Class<? extends Command> defaultCommand();
 }
