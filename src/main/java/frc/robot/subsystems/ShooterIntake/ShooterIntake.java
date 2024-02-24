d2cfb8766d66d9bb14e7019581e6f90e58666fb9
lesserc <cdlesser@provide.net>
2024-02-24 15:17:57 -0500

    Shooter Gain changes

--- src/main/java/frc/robot/subsystems/ShooterIntake/ShooterIntake.java
+++ src/main/java/frc/robot/subsystems/ShooterIntake/ShooterIntake.java
@@ -30,7 +30,7 @@ final class ShooterIntakeConstants{
   public final static DigitalInput PHOTO_SENSOR_NO = new DigitalInput(PHOTO_SENSOR_NO_CHANNEL);
   public final static DigitalInput PHOTO_SENSOR_NC = new DigitalInput(PHOTO_SENSOR_NC_CHANNEL);
 
-  public final static double SHOOTER_DEFAULT_RPM = 4500;
+  public final static double SHOOTER_DEFAULT_RPM = 4000;
   public final static double SHOOTER_HOLD_RPM = -100;
   public final static double INTAKE_DEFAULT_PICK_UP_RPM = 2500;
   public final static double INTAKE_DEFAULT_INDEX_RPM = 100;
