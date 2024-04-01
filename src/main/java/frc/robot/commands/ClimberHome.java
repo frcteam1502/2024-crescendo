68db769d2f2068d49aa9228b2e20ce3c69134210
Team 1502 <frcteam1502@gmail.com>
2024-04-01 17:36:28 -0400

    emilychanges

--- src/main/java/frc/robot/commands/ClimberHome.java
+++ src/main/java/frc/robot/commands/ClimberHome.java
@@ -29,7 +29,9 @@ public class ClimberHome extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    climber.climberOff();
+  }
 
   // Returns true when the command should end.
   @Override
